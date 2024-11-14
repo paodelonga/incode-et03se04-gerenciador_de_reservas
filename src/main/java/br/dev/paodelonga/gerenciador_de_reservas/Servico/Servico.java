f8bcb1497ec003a3d60964c7979cdd664ee72a8c
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-11-14 05:24:50 -0300

    refactor: padroniza return tipos
    Fala agora que não tem retorno, fala dele fala
    
    Signed-off-by: paodelonga <72471292+paodelonga@users.noreply.github.com>

--- src/main/java/br/dev/paodelonga/gerenciador_de_reservas/Servico/Servico.java
+++ src/main/java/br/dev/paodelonga/gerenciador_de_reservas/Servico/Servico.java
@@ -7,6 +7,8 @@ import br.dev.paodelonga.gerenciador_de_reservas.Entidade.Reserva;
 import br.dev.paodelonga.gerenciador_de_reservas.Tipo.QuartoNome;
 
 import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class Servico {
     Hotel hotel;
@@ -19,49 +21,51 @@ public class Servico {
     public void alterarNomeHotel(String nome) {}
 
     // Adders
-    public void adicionarReserva(Reserva reserva) {}
-    public void adicionarQuarto(Quarto quarto) {}
-    public void adicionarHospede(Hospede hospede) {}
-    public void adicionarQuartoTabela(QuartoNome nome, BigDecimal valor, Integer quantidade) {}
+    public Reserva adicionarReserva(Reserva reserva) {}
+    public Quarto adicionarQuarto(Quarto quarto) {}
+    public Hospede adicionarHospede(Hospede hospede) {}
+    public boolean adicionarQuartoTabela(QuartoNome nome, BigDecimal valor, Integer quantidade) {}
 
     // Obters
-    public void obterNomeHotel() {}
-    public void obterReserva(Reserva reserva) {}
-    public void obterQuarto(Quarto quarto) {}
-    public void obterHospede(Hospede hospede) {}
-    public void obterQuartoTabela(QuartoNome nome) {}
+    public String obterNomeHotel() {}
+    public Reserva obterReserva(Reserva reserva) {}
+    public Quarto obterQuarto(Quarto quarto) {}
+    public Hospede obterHospede(Hospede hospede) {}
+    public Map<BigDecimal, Integer> obterQuartoTabela(QuartoNome nome) {}
 
     // Listers
-    public void obterListaReservas() {}
-    public void obterListaQuartos() {}
-    public void obterListaHospedes() {}
-    public void obterListaQuartosTabela() {}
+    public LinkedList<Reserva> obterListaReservas() {}
+    public LinkedList<Quarto> obterListaQuartos() {}
+    public LinkedList<Hospede> obterListaHospedes() {}
+    // E agora voce se pergunta: ah, por que não temos um `obterListaQuartosTabela`?
+    // é que... gafanhato, fudeu!
 
     // Removers
-    public void removerReserva(Reserva reserva) {}
-    public void removerQuarto(Quarto quarto) {}
-    public void removerHospede(Hospede hospede) {}
-    public void removerQuartoTabela(QuartoNome nome) {}
+    public Reserva removerReserva(Reserva reserva) {}
+    public Quarto removerQuarto(Quarto quarto) {}
+    public Hospede removerHospede(Hospede hospede) {}
+    public QuartoNome removerQuartoTabela(QuartoNome nome) {}
 
     // Coisitas externas
-    public void alugarQuarto(Hospede hospede, Quarto quarto) {
+    // Só pra deixar claro, esse aqui é o cara que faz a reserva no "front"
+    public Quarto alugarQuarto(Hospede hospede, Quarto quarto) {
     }
 
-    public void consultarReserva(Reserva reserva) {
+    public Reserva consultarReserva(Reserva reserva) {
     }
 
-    public void cancelarReserva(Reserva reserva) {
+    public Reserva cancelarReserva(Reserva reserva) {
     }
 
-    public void pagarReserva(Reserva reserva) {
+    public Reserva pagarReserva(Reserva reserva) {
     }
 
-    public void fazerCheckIn(Reserva reserva) {
+    public Reserva fazerCheckIn(Reserva reserva) {
     }
 
-    public void fazerCheckOut(Reserva reserva) {
+    public Reserva fazerCheckOut(Reserva reserva) {
     }
 
-    public void listarReservas() {
+    public LinkedList<Reserva> listarReservas() {
     }
 }
